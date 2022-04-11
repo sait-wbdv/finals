7c8b9f66447c14127e387629663dbdcb16eccb69
Tony Grimes <acidtone@tonygrimes.com>
2022-04-11 13:33:57 -0600

    Initial commit

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,7 @@
+<header>
+  <img src="images/sait-logo.png" alt="SAIT Logo">
+  <div>
+    <h1>SAIT Web Developer Fast Track</h1>
+    <h2>Final Presentations - December 17, 2021</h2>
+  </div>
+</header>
