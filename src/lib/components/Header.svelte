68b7a5d015117f781bb1ca6f11b179c619fab102
Tony Grimes <acidtone@tonygrimes.com>
2022-04-21 09:27:32 -0600

    added css and sait logo

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,5 @@
 <header>
-  <img src="images/sait-logo.png" alt="SAIT Logo">
+  <img src="/assets/images/sait-logo.png" alt="SAIT Logo">
   <div>
     <h1>SAIT Web Developer Fast Track</h1>
     <h2>Final Presentations - December 17, 2021</h2>
