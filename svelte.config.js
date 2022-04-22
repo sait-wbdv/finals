f3b22b9a98f5823d7f37079debc6da52c3acf71e
Tony Grimes <acidtone@tonygrimes.com>
2022-04-22 07:31:24 -0600

    gh pages troubleshooting

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,11 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess(),
 	kit: {
+		target: '#svelte',
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
